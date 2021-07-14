3cab8e9bd23a0e86a5806cc0c0913de3259ce48c
adzh_ <56293503+adzh2100@users.noreply.github.com>
2021-07-14 16:58:31 +0300

    Update Euler.m

--- Euler.m
+++ Euler.m
@@ -1,12 +1,12 @@
 function Euler
  
-	% Инициализираме параметрите, които ще използваме
+	% Initializing parameters
 	x0=2; y0=1; b=4;
 	h1=0.4; h2=0.2 ; h3=0.02;
     xmin=2;
     xmax=4;
     
-    % Чертаем графиката на решението с черен цвят
+    % Plotting the solution
     hold on;
     grid on;
     axis([xmin, xmax, -5, 60]);
@@ -14,7 +14,7 @@ function Euler
     x=linspace(xmin,xmax);
     plot(x,eval(y),'k');
     
-	% Инициализираме масивите
+	% Initializing the arrays
 	x1=[];y1=[];
 	x1(1)=x0;y1(1)=y0;
 	
@@ -26,12 +26,12 @@ function Euler
 	
 	N=(b-x0);
      
-    % Дефинираме функцията
+    % Defining main function
 	function z=ff(x,y)
 		z=(3*y+x^3)/x;
     end
 
-	% Попълваме масивите
+	% Filling the arrays
 	for m=1:(N/h1)
         x1(m+1)=x0+m*h1;
         y1(m+1)=y1(m)+ff(x1(m),y1(m))*h1; 
@@ -47,7 +47,7 @@ function Euler
 		y3(j+1)=y3(j)+ff(x3(j),y3(j))*h3;
     end
     
-	% Чертаем графиките приближенията
+	% Plotting the approximations
 	plot(x1,y1,'g',x2,y2,'b',x3,y3,'r')
  
 end
